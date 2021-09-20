99c2322135384e4f5bee3e5c7370ee956d9144b3
elabd34 <abdellah.benmouna@gmail.com>
2021-09-20 16:05:47 +0200

    views

---
+++ ncdropzone-boot/src/main/java/fr/projet/api/Views.java
@@ -0,0 +1,17 @@
+package fr.projet.api;
+
+public interface Views {
+    public static interface Commons{}
+
+    public static interface Avion extends Commons{}
+
+    public static interface Parachute extends Commons{}
+
+    public static interface Parachutiste extends Commons{}
+
+    public static interface Pilote extends Commons{}
+
+    public static interface Saut extends Commons{}
+
+    public static interface Vol extends Commons{}
+}
