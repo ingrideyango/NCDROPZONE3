88c6d99080e8e13da24f09e4521d668a42f3f191
ingrideyango <ingruide@hotmail.fr>
2021-09-20 10:48:40 +0200

    debut projet

---
+++ ncdropzone-boot/src/main/java/fr/projet/ncdropzoneboot/NcdropzoneBootApplication.java
@@ -0,0 +1,13 @@
+package fr.projet.ncdropzoneboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcdropzoneBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcdropzoneBootApplication.class, args);
+	}
+
+}
---
+++ ncdropzone-boot/src/test/java/fr/projet/ncdropzoneboot/NcdropzoneBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.ncdropzoneboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcdropzoneBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
