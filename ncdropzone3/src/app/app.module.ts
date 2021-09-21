28475193e5b0a0e39d2d65682e11c2450d7033e1
PierreSchickele <pierre.schickele@live.fr>
2021-09-21 11:09:09 +0200

    modif appmodule import httpclient

--- ncdropzone3/src/app/app.module.ts
+++ ncdropzone3/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,6 +35,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     NgbModule
   ],
   providers: [],
