f1a1d18fcd3f6b0b7a88897291abf8415c37f8a3
PierreSchickele <pierre.schickele@live.fr>
2021-09-21 10:54:00 +0200

    ajout formModule

--- ncdropzone3/src/app/app.module.ts
+++ ncdropzone3/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
