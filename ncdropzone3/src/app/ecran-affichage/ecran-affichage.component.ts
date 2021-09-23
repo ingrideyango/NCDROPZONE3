791c883299cb157cc3be8282ad08b39821b9045b
benoit <benoit.chauvin@estaca.eu>
2021-09-23 16:34:56 +0200

    ecran affichage

--- ncdropzone3/src/app/ecran-affichage/ecran-affichage.component.ts
+++ ncdropzone3/src/app/ecran-affichage/ecran-affichage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModuleAvionnageService } from '../module-avionnage.service';
 import { ParachutisteService } from '../parachutiste.service';
+import { TandemService } from '../tandem.service';
 import { VolService } from '../vol.service';
 
 @Component({
@@ -12,15 +14,13 @@ vols: any = [];
 vol: any = {};
 parachutistes: any = [];
 parachutiste: any = {};
+tandems: any = [];
 
 
-  constructor(
-    private srvVol: VolService,
-    private srvParachutiste: ParachutisteService,
-  
-  ) { 
+  constructor(private srvVol: VolService, private srvAvionnage: ModuleAvionnageService, private srvTandem: TandemService  ) { 
     this.vols = this.srvVol.findAll();
-    this.parachutistes = this.srvParachutiste.findAll();
+    this.parachutistes = this.srvAvionnage.findAllAvionnage();
+    this.tandems = this.srvTandem.findAll();
   
   }
 
